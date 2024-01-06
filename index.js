d21b4346e4586ef6a3ed342e7d94d8d833ddcb44
Jiří Kuneš <jirka642@gmail.com>
2024-01-06 19:48:22 +0100

    more colors

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const artistIndex = [];
 
+function hsvToRgb(h, s, v) {
+  let r, g, b;
+
+  const i = Math.floor(h * 6),
+      f = h * 6 - i,
+      p = v * (1 - s),
+      q = v * (1 - f * s),
+      t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0: r = v; g = t; b = p; break;
+    case 1: r = q; g = v; b = p; break;
+    case 2: r = p; g = v; b = t; break;
+    case 3: r = p; g = q; b = v; break;
+    case 4: r = t; g = p; b = v; break;
+    case 5: r = v; g = p; b = q; break;
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
+
 class ArtistData {
+    static hueMin = 0.0;
+    static hueMax = 1.0;
+    static hueStep = (ArtistData.hueMax - ArtistData.hueMin) / 20;
+    static hueMap = {};
+
+    static saturationMin = 0.4;
+    static saturationMax = 1.0;
+    static saturationStep = (ArtistData.saturationMax - ArtistData.saturationMin) / 6;
+    static saturationMaps = {};
+
     constructor (path) {
         this.path = path;
         this._data = null;
         this._chartData = null;
+        this._chartColor = null
     }
 
     async getData() {
@@ -26,24 +58,50 @@ class ArtistData {
     }
 
     get chartColor() {
-        if (this._data) {
-            switch (this._data['branch'] && this._data['branch'].toLowerCase()) {
-                case 'jp':
-                    return '#cc0000';
-                case 'id':
-                    return '#cc00cc';
-                case 'en':
-                    return '#00cc00';
-                case 'dev_is':
-                    return '#00cccc';
-                case null:
-                    return '#cccccc';
-                default:
-                    return '#000000';
+        if (this._chartColor) {
+            return this._chartColor;
+        }
+        else if (this._data) {
+            const hueKey = `${this._data['branch']};${this._data['generation']}`,
+                saturationKey = `${this._data['name']}`;
+
+            if (!(hueKey in ArtistData.hueMap)) {
+                let newHue = ArtistData.hueMin + ArtistData.hueStep * Object.keys(ArtistData.hueMap).length;
+
+                while (newHue > ArtistData.hueMax) {
+                    newHue -= ArtistData.hueMax - ArtistData.hueMin;
+                }
+
+                ArtistData.hueMap[hueKey] = newHue;
             }
+
+            if (!(hueKey in ArtistData.saturationMaps)) {
+                ArtistData.saturationMaps[hueKey] = {};
+            }
+
+
+            if (!(saturationKey in ArtistData.saturationMaps[hueKey])) {
+                let newSaturation = (
+                    ArtistData.saturationMin
+                    + ArtistData.saturationStep * Object.keys(ArtistData.saturationMaps[hueKey]).length
+                );
+
+                while (newSaturation > ArtistData.saturationMax) {
+                    newSaturation -= ArtistData.saturationMax - ArtistData.saturationMin;
+                }
+
+                ArtistData.saturationMaps[hueKey][saturationKey] = newSaturation;
+            }
+
+            const [r, g, b] = hsvToRgb(
+                ArtistData.hueMap[hueKey], ArtistData.saturationMaps[hueKey][saturationKey], 0.9
+            );
+
+            this._chartColor = `rgb(${r}, ${g}, ${b})`;
+            return this._chartColor;
         }
         else {
-            return '#000000';
+            return 'rgb(0, 0, 0)';
         }
     }
 
@@ -96,7 +154,8 @@ function initChart() {
                 get borderColor() {
                     return artistData.chartColor;
                 },
-                fill: false
+                hidden: false,
+                fill: false,
             }
         })
     };
@@ -113,6 +172,15 @@ function initChart() {
                         display: true,
                         text: 'Hololive Spotify - Monthly Listeners'
                     },
+                    legend: {
+                        display: true,
+                        labels: {
+                            filter: function(item, chart) {
+                                // Don't show datasets that are not ready in legend
+                                return item.text !== '???';
+                            }
+                        }
+                    },
                 },
                 interaction: {
                     intersect: false,
@@ -124,6 +192,9 @@ function initChart() {
                             unit: 'month',
                             parser: 'YYYY-MM-DD'
                         }
+                    },
+                    y: {
+                        min: 0
                     }
                 }
             },
@@ -137,7 +208,13 @@ function initChart() {
     const chart = initChart();
 
     for (const artistData of artistIndex) {
-        await artistData.getData();
+        try {
+            await artistData.getData();
+        }
+        catch (e) {
+            console.error('Could not fetch:', artistData.path);
+            continue;
+        }
         chart.update();
     }
 })();
