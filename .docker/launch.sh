c903b13a9ec1c0d246325821a8da0b64bf087c69
Jiří Kuneš <jirka642@gmail.com>
2025-03-29 22:58:12 +0000

    [FIX] Run with same UID+GID as host user

--- .docker/launch.sh
+++ .docker/launch.sh
@@ -3,4 +3,7 @@ set -e
 
 DOCKER_SRC="/opt/holo-spotify-stats"
 
-cd "$DOCKER_SRC" && python cron.py
+groupadd -g "$GID" holo-spotify-stats \
+&& useradd holo-spotify-stats -u "$UID" -g "$GID" -m -s /bin/bash \
+&& cd "$DOCKER_SRC" \
+&& sudo -u holo-spotify-stats python cron.py
