71557d255d02a4b0afcaf7d3b1838534b1b47b1e
Jiří Kuneš <jirka642@gmail.com>
2025-07-03 23:15:13 +0200

    [FIX] Remove --with-deps

--- .docker/launch.sh
+++ .docker/launch.sh
@@ -8,6 +8,6 @@ groupadd --force -g "$GID" host
 useradd host -u "$UID" -g "$GID" -s /bin/bash --home /home/host/ || true
 
 # install the playwright browser for the current user
-sudo -u host playwright install --with-deps chromium-headless-shell
+sudo -u host playwright install chromium-headless-shell
 
 cd /home/host/holo-spotify-stats && sudo -u host python cron.py
