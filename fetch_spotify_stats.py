6b3d5f95eb31189e5d78cbfc6cd798fdc8fc5075
Jiří Kuneš <jirka642@gmail.com>
2025-07-03 22:57:11 +0200

    [FIX] Get token with playwright

--- fetch_spotify_stats.py
+++ fetch_spotify_stats.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-import base64
 import datetime
 import glob
-import hashlib
 import json
 import logging
 import os
-import re
 import time
 import uuid
 
-import pyotp
+import playwright
+import playwright.sync_api
 import requests
 
 _logger = logging.getLogger(__name__)
@@ -76,125 +74,24 @@ def request_retry(*args, retry_max_count: int = 3, retry_delay: int = 10, **kwar
     return response
 
 
-def spotify_decode_secret(raw_secret: list[int]) -> str:
-    """
-    Raw secret can be found in assets.
-    {
-      "validUntil":"2025-07-02T12:00:00.000Z",
-      "secrets":[
-        {"secret":[37,84,32,76,87,90,87,47,13,75,48,54,44,28,19,21,22],"version":8},
-        {"secret":[59,91,66,74,30,66,74,38,46,50,72,61,44,71,86,39,89],"version":7},
-        {"secret":[21,24,85,46,48,35,33,8,11,63,76,12,55,77,14,7,54],"version":6}
-      ]
-    }
-
-    Example input:
-        [37,84,32,76,87,90,87,47,13,75,48,54,44,28,19,21,22]
-    Example output:
-        GQ2DSNBUGM3DIOJQHA2DQOBWGMZDQOBZGM2TGNBVG4YTANBRGMYTK
-    """
-    k = [(e ^ t % 33 + 9) for t, e in enumerate(raw_secret)]
-    uint8_secret = [int(x) for x in "".join([str(x) for x in k]).encode("utf-8")]
-    bytes_secret = bytes(uint8_secret)
-    return base64.b32encode(bytes_secret).decode("ascii")
-
-
-def spotify_totp(decoded_secret: str, timestamp: float) -> str:
-    """
-    Returns totp value used by spotify
-    :param decoded_secret: output of spotify_decode_secret
-    :param timestamp: current time in seconds
-    """
-    period = 30
-    return pyotp.hotp.HOTP(
-        s=decoded_secret,
-        digits=6,
-        digest=hashlib.sha1,
-    ).at(int(timestamp / period))
-
-
-def get_spotify_secrets() -> list[dict]:
-    global SECRETS, SECRETS_EXPIRY
-
-    if not SECRETS or SECRETS_EXPIRY > time.time():
-        # get url of assets
-
-        response = request_retry(
-            method="GET",
-            url="https://open.spotify.com",
-            headers={
-                "User-Agent": (
-                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                    "(KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
-                ),
-            },
-        )
-        response.raise_for_status()
-
-        match = re.search(r"\"([\w/:\-\.]*/web-player\.\w*\.js)\"", response.content.decode("utf-8"))
-        if not match:
-            raise ValueError("Url of assets could not be found")
-        assets_url = match.group(1)
-
-        # download assets and parse the json with secrets
-
-        response = request_retry(method="GET", url=assets_url)
-        response.raise_for_status()
-
-        match = re.search(r"\'({\"validUntil\":[^']*)\'", response.content.decode("utf-8"))
-        if not match:
-            raise ValueError("Could not find secrets in assets")
-
-        data = json.loads(match.group(1))
-        SECRETS = data["secrets"]
-        SECRETS_EXPIRY = datetime.datetime.fromisoformat(data["validUntil"].split(".")[0]).timestamp()
-
-    return SECRETS
-
-
 def get_auth_token() -> str:
     global AUTH_TOKEN, AUTH_TOKEN_EXPIRY
 
     if not AUTH_TOKEN or AUTH_TOKEN_EXPIRY > time.time():
-        secrets = get_spotify_secrets()
-        decoded_secret = spotify_decode_secret(secrets[0]["secret"])
-        version = secrets[0]["version"]
+        with playwright.sync_api.sync_playwright() as pw:
+            browser = pw.chromium.launch(headless=True)
+            context = browser.new_context(viewport={"width": 1920, "height": 1080})
+            page = context.new_page()
 
-        c_time = int(time.time() * 1000)
-        # s_time = c_time // 1000
-        totp = totp_server = spotify_totp(decoded_secret, c_time / 1000)
+            with page.expect_response(
+                lambda response: "/api/token" in response.url and response.ok, timeout=30 * 1000
+            ) as response_info:
+                page.goto("https://open.spotify.com")
+                data = response_info.value.json()
+                AUTH_TOKEN = data["accessToken"]
+                AUTH_TOKEN_EXPIRY = data["accessTokenExpirationTimestampMs"] / 1000
 
-        response = request_retry(
-            method="GET",
-            url="https://open.spotify.com/api/token",
-            params={
-                "reason": "init",
-                "productType": "web-player",
-                "totp": totp,
-                "totpServer": totp_server,
-                "totpVer": version,
-            },
-            headers={
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "dnt": "1",
-                "priority": "u=1, i",
-                "referer": "https://open.spotify.com/",
-                # 'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
-                # 'sec-ch-ua-mobile': '?0',
-                # 'sec-ch-ua-platform': '"Linux"',
-                # 'sec-fetch-dest': 'empty',
-                # 'sec-fetch-mode': 'cors',
-                # 'sec-fetch-site': 'same-origin',
-                # 'sentry-trace': ,
-                # 'user-agent': ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                # '(KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'),
-            },
-        )
-
-        data = response.json()
-        AUTH_TOKEN = data["accessToken"]
-        AUTH_TOKEN_EXPIRY = data["accessTokenExpirationTimestampMs"] / 1000
+            browser.close()
 
         _logger.debug("Auth token: %s", AUTH_TOKEN)
 
