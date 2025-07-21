070873207705e9e95b422c945752304caa420c83
Jiří Kuneš <jirka642@gmail.com>
2025-07-21 20:57:16 +0200

    [IMP] Hide groups by default

--- website/src/state.js
+++ website/src/state.js
@@ -3,6 +3,12 @@ import * as owl from '@odoo/owl';
 export const STATE = owl.reactive({
     artistIndex: [],
     hiddenBranches: new Set([
-        'INoNaKa Music'  // not relevant
+        // Not relevant + is subset of "Hololive JP"
+        // - is completely removed from UI in stats_chart.js
+        'INoNaKa Music',
+        // Don't show group projects by default. Most people care only about the talent channels.
+        'Other',
+        // Don't show non-holo channels by default
+        'Independent'
     ])
 });
